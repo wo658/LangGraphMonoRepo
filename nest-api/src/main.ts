6ac606982bcdc3cdb6db92d99a869c951ba26e36
wo658 <whdgns2347@gmail.com>
2025-08-14 08:34:32 +0900

    포트변경

--- nest-api/src/main.ts
+++ nest-api/src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
   // Enable cookie parser
   app.use(cookieParser());
   
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 4000;
   await app.listen(port);
   console.log(`Application is running on: http://localhost:${port}`);
 }
